426543239f61afdf50c3aef2d599d5245781f497
Taahia23 <taahiatahsin@gmail.com>
2024-01-28 03:15:08 +0600

    implemented total price

--- src/layout/UserAdminDashboard.jsx
+++ src/layout/UserAdminDashboard.jsx
@@ -1,29 +1,36 @@
-import {  FaHome, FaShoppingCart } from "react-icons/fa";
+import { FaHome, FaShoppingCart, FaUser } from "react-icons/fa";
 import { PiNotebookLight } from "react-icons/pi";
 import { NavLink, Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar";
 
 const UserAdminDashboard = () => {
     return (
-        <div className="flex">
-            <div className="w-64 min-h-screen bg-[#ffeaa7]">
-                <ul className="menu p-5 space-y-3">
-                    
-                    <li className="text-lg">
-                        <NavLink to={'/dashboard/userHome'}><FaHome></FaHome> User Profile</NavLink>
-                    </li>
-                    <li className="text-lg">
-                        <NavLink to={'/dashboard/cart'}> <FaShoppingCart></FaShoppingCart> My Cart</NavLink>
-                    </li>
-                    <li className="text-lg">
-                        <NavLink to={'/dashboard/review'}><PiNotebookLight /> Add Review</NavLink>
-                    </li>
-                    <li> <NavLink to={'/'}></NavLink></li>
-                    <li> <NavLink to={'/'}></NavLink></li>
+        <div>
+            <Navbar></Navbar>
+            <div className="flex">
+                <div className="w-64 min-h-screen bg-[#ffeaa7]">
+                    <ul className="menu p-5 space-y-3">
 
-                </ul>
-            </div>
-            <div className="flex-1">
-                <Outlet></Outlet>
+                        <li className="text-lg">
+                            <NavLink to={'/dashboard/userHome'}><FaUser className="mr-2"></FaUser>User Profile</NavLink>
+                        </li>
+                        <li className="text-lg">
+                            <NavLink to={'/dashboard/cart'}> <FaShoppingCart className="mr-2"></FaShoppingCart> My Cart</NavLink>
+                        </li>
+                        <li className="text-lg">
+                            <NavLink to={'/dashboard/review'}><PiNotebookLight className="mr-2" /> Add Review</NavLink>
+                        </li>
+                        <div className="divider divider-neutral"></div>
+                        <li className="text-lg">
+                            <NavLink to={'/'}><FaHome className="mr-2"></FaHome>Home</NavLink>
+                        </li>
+                        <li> <NavLink to={'/'}></NavLink></li>
+
+                    </ul>
+                </div>
+                <div className="flex-1">
+                    <Outlet></Outlet>
+                </div>
             </div>
         </div>
     );
--- src/pages/UserAdminDashboard/MyCart.jsx
+++ src/pages/UserAdminDashboard/MyCart.jsx
@@ -1,8 +1,68 @@
+import { key } from "localforage";
+import useCart from "../../hooks/useCart";
 
 const MyCart = () => {
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
     return (
         <div>
-            my cart
+            <h1 className="text-5xl">My Cart</h1>
+            <div className="flex justify-evenly">
+                <h1 className="text-4xl">Total items : {cart.length}</h1>
+                <h1 className="text-4xl">Total price : {totalPrice} tk</h1>
+                <button className="btn btn-ghost">Pay</button>
+            </div>
+
+            <div className="overflow-x-auto p-10">
+                <table className="table text-lg ">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                              #
+                            </th>
+                            <th>Name</th>
+                            
+                            <th>price</th>
+                            <th>Category</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item,idx)  => <tr key={item._id}>
+                                <th>{idx+1} </th>
+                                {/* <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                               <img src={item.productImage} alt="" />
+
+                                            </div>
+                                        </div>
+                                        <div>
+                                           
+                                            
+                                        </div>
+                                    </div>
+                                </td> */}
+                                <td> <div className="font-bold">{item.productName}</div></td>
+                                <td>
+                                    {item.price} tk
+                                    
+                                </td>
+                                <td>{item.category}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">Delete</button>
+                                </th>
+                            </tr>)
+                        }
+                        {/* row 1 */}
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
